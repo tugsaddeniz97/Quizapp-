5ed122f759d8ba4a01448310e91a0398d486d808
Giorgi Gogeshvili <giorgi.gogeshvili@hamburger-software.de>
2026-04-02 10:11:10 +0200

    add CORS deactivation in backened

--- Backend/QuizApp/Program.cs
+++ Backend/QuizApp/Program.cs
@@ -4,6 +4,18 @@ using QuizApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(
+    options =>
+    {
+        options.AddPolicy("AddFrontEnd", policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+    }
+    );
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -16,6 +28,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=quiz.db"));
 
 var app = builder.Build();
+
+app.UseCors("AddFrontEnd");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
